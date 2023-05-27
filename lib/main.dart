0d060f8017f414828b3812452a37c5d08e8afb60
Vladyslav Petlovyi <>
2023-05-27 15:08:55 +0300

    added logic for logged and not logged users

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:chatting_app/helper/helper_function.dart';
+import 'package:chatting_app/pages/auth/login_page.dart';
+import 'package:chatting_app/pages/home_page.dart';
 import 'package:chatting_app/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -19,14 +22,35 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunction.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = value;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: _isSignedIn ?  const HomePage() :  const LoginPage(),
     );
   }
 }
