5263423a15d5d169813491667eeb8d728df79acf
Vladyslav Petlovyi <>
2023-05-27 16:56:17 +0300

    aded  image to login screen

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -8,10 +8,30 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Login page")),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment:  CrossAxisAlignment.center,
+              children: <Widget> [
+                const Text("BlaBlaChat",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10,),
+                const Text("Login now to express your blablablas!", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400)),
+                Image.asset("assets/login-talking.jpg")
+              ],
+            )
+          ),
+        ),
+      ),
     );
   }
 }
